e6b66320fa204d9f2019375550092054c1de5f42
broxzx <illyasedelnikov123@gmail.com>
2026-01-04 17:36:03 +0100

    problem 1390

--- src/main/java/com/fyuizee/App.java
+++ src/main/java/com/fyuizee/App.java
@@ -1,7 +1,10 @@
 package com.fyuizee;
 
+import com.fyuizee.p1390.Solution;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Solution solution = new Solution();
+    System.out.println(solution.sumFourDivisors(new int[] { 21, 4, 7 }));
   }
 }
---
+++ src/main/java/com/fyuizee/p1390/Solution.java
@@ -0,0 +1,51 @@
+package com.fyuizee.p1390;
+
+/*
+Given an integer array nums, return the sum of divisors of the integers in that array that have exactly four divisors. If there is no such integer in the array, return 0. 
+
+Example 1:
+
+Input: nums = [21,4,7]
+Output: 32
+Explanation: 
+21 has 4 divisors: 1, 3, 7, 21
+4 has 3 divisors: 1, 2, 4
+7 has 2 divisors: 1, 7
+The answer is the sum of divisors of 21 only.
+Example 2:
+
+Input: nums = [21,21]
+Output: 64
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 0
+*/
+
+public class Solution {
+
+  public int sumFourDivisors(int[] nums) {
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += factors(nums[i]);
+    }
+    return sum;
+
+  }
+
+  private int factors(int n) {
+    int sum = 0, c = 0;
+    for (int i = 2; i * i <= n; i++) {
+      if (n % i == 0) {
+        int j = n / i;
+        if (j == i || c > 0)
+          return 0;
+        sum += i + j;
+        c++;
+      }
+    }
+    if (c == 0)
+      return 0;
+    return 1 + sum + n;
+  }
+}
