192d07de021ab2504cfdc43ab0e9a33ab8a73281
broxzx <illyasedelnikov123@gmail.com>
2026-01-24 21:32:58 +0100

    new problems solved

--- src/main/java/com/fyuizee/p143/Solution.java
+++ src/main/java/com/fyuizee/p143/Solution.java
@@ -1,7 +1,5 @@
 package com.fyuizee.p143;
 
-import java.util.Stack;
-
 import com.fyuizee.utils.ListNode;
 import com.fyuizee.utils.ListNodeUtil;
 
@@ -14,36 +12,54 @@ public class Solution {
     ListNode test = ListNodeUtil.buildList(new int[] { 1, 2, 3, 4 });
     reorderList(test);
     System.out.println(test);
+
+    ListNode test2 = ListNodeUtil.buildList(new int[] { 1, 2, 3, 4, 5 });
+    reorderList(test2);
+    System.out.println(test2);
   }
 
   public static void reorderList(ListNode head) {
-    ListNode cur = head;
-    Stack<ListNode> uneven = new Stack<>();
-    int count = 0;
-
-    while (cur != null) {
-      if (count % 2 != 0) {
-        uneven.add(new ListNode(cur.val));
-        count++;
-      }
-      count++;
-      cur = cur.next;
+    if (head == null || head.next == null) {
+      return;
     }
 
-    for (ListNode node : uneven) {
-      System.out.println(node);
+    ListNode slow = head, fast = head, prev = null;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      prev = slow;
+      slow = slow.next;
     }
 
-    cur = head;
-    count = 0;
-    while (cur != null) {
-      if (count++ % 2 == 0 && cur.next != null && !uneven.isEmpty()) {
-        ListNode pop = uneven.pop();
-        pop.next = cur.next;
-        cur.next = pop;
-      }
+    prev.next = null;
+    ListNode tail = reverseNode(slow);
 
-      cur = cur.next;
+    ListNode currentHead = head, temp = head;
+    while (currentHead != null && tail != null) {
+      ListNode next = tail;
+      tail = tail.next;
+
+      next.next = currentHead.next;
+      currentHead.next = next;
+      temp = currentHead.next;
+      currentHead = currentHead.next.next;
     }
+
+    if (tail != null) {
+      temp.next = tail;
+    }
+  }
+
+  private static ListNode reverseNode(ListNode node) {
+    ListNode prev = null, current = node;
+
+    while (current != null) {
+      ListNode next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    return prev;
   }
 }
